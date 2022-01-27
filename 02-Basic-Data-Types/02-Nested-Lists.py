00f9e71d3995dba36f10bde56f9a76ca82c46363
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2022-01-26 21:29:46 -0300

    Refactoring 'Nested-Lists.py' at 'Basic-Data-Types'.

--- 02-Basic-Data-Types/02-Nested-Lists.py
+++ 02-Basic-Data-Types/02-Nested-Lists.py
@@ -6,10 +6,9 @@ def main():
     n = int(input())
     records = [[input(), float(input())] for _ in range(n)]
 
-    scores = [list_element[1] for list_element in records]
-    scores = [score for score in scores if score > min(scores)]
-
-    students = [list_element[0] for list_element in records if list_element[1] == min(scores)]
+    students_score = [list_element[1] for list_element in records]
+    second_lowest_score = sorted(list(set(students_score)))[1]
+    students = [list_element[0] for list_element in records if list_element[1] == second_lowest_score]
     students.sort()
 
     [print(name) for name in students]
